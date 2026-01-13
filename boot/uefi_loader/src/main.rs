6870b8589efaef3a68595d0b2ba1e0dc9765c5de
Sindre Stephansen <sindre@sindrestephansen.com>
2026-01-13 20:30:30 +0100

    Add basic UEFI executable code

--- boot/uefi_loader/src/main.rs
+++ boot/uefi_loader/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use core::time::Duration;
+
+use log::info;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+
+    system::with_stdout(|s| s.clear().unwrap());
+    info!("Hello World!");
+    boot::stall(Duration::from_secs(10));
+
+    Status::SUCCESS
 }
---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+script_path="${BASH_SOURCE[0]}"
+script_dir="$(dirname "$script_path")"
+root_dir="$script_dir/.."
+
+cd "$root_dir" || exit 1
+
+cargo build --target x86_64-unknown-uefi || exit 1
+
+cp "$root_dir/target/x86_64-unknown-uefi/debug/uefi_loader.efi" "$root_dir/tmp/esp/efi/boot/bootx64.efi" || exit 1
+
+
+qemu-system-x86_64 \
+    -enable-kvm \
+    -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_CODE.4m.fd \
+    -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/x64/OVMF_VARS.4m.fd \
+    -drive format=raw,file=fat:rw:"$root_dir/tmp/esp"
